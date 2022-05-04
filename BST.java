be07708a4afdea2fa1b13897e0b91172abd73831
tuchaVshortah <nukenbai190104@gmail.com>
2022-05-04 11:53:43 +0600

    Delete HashNode file

--- BST.java
+++ BST.java
@@ -1,2 +1,16 @@
-public class BST {
+public class BST<K extends Comparable<K>, V> {
+    private Node root;
+    private class Node {
+        private K key;
+        private V value;
+        private Node left, right;
+        public Node(K key, V val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+    public void put(K key, V val) {}
+    public V get(K key) {}
+    public void delete(K key) {}
+    public Iterable<K> iterator() {}
 }
--- MyHashTable.java
+++ MyHashTable.java
@@ -1,6 +1,30 @@
 public class MyHashTable<K, V> {
 
     private class HashNode<K, V> {
-        private 
+        private K key;
+        private V value;
+        private HashNode <K, V> next;
+
+        public HashNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "{" + key + " " + value + "}";
+        }
     }
+
+    private HashNode<K, V>[] chainArray;
+    private int M = 11;
+    private int size
+    public MyHashTable() {}
+    public MyHashTable(int M) {}
+    private int hash(K key) {}
+    public void put(K key, V value) {}
+    public V get(K key) {}
+    public V remove(K key) {}
+    public boolean contains(V value) {}
+    public K getKey(V value) {}
 }

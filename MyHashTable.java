81d84609492e0953466e4ef58820a97a83d95a8f
tuchaVshortah <nukenbai190104@gmail.com>
2022-05-07 20:45:55 +0600

    Define the put(Key K, V value) method in of MyHashTable class

--- MyHashTable.java
+++ MyHashTable.java
@@ -4,7 +4,7 @@ public class MyHashTable<K, V> {
 
         public K key;
         public V value;
-        public HashNode <K, V> next;
+        public HashNode <K, V> next = null;
 
         public HashNode(K key, V value) {
             this.key = key;
@@ -21,7 +21,11 @@ public class MyHashTable<K, V> {
     private int M = 11;
     private int size = 0;
 
-    public MyHashTable() {}
+    public MyHashTable() {
+        for(int i = 0; i < M; i++){
+            chainArray[i] = null;
+        }
+    }
 
     public MyHashTable(int M) {
         this.M = M;
@@ -35,17 +39,54 @@ public class MyHashTable<K, V> {
     }
 
     public void put(K key, V value) {
-        /*
-        int bucketIndex = hash(key) % M < 0 index * (-1) : index;
-        int hashCode = hash(key);
-        HashNode<K, V> node = chainArray[bucketIndex];
 
+        int chainIndex = hash(key) % M < 0 index * (-1) : index;
+        int hashCode = hash(key);
+        HashNode<K, V> node = chainArray[chainIndex];
+
+        //check if key is already in the hashtable
         while(node != null){
             if(node.key.equals(key)) {
+                node.value = value;
+                return;
+            }
+            node = node.next;
+        }
 
+        //otherwise increase size of the hashtable and add it
+        size++;
+
+        node = chainArray[chainIndex];
+        HashNode<K, V> newNode = new HashNode<>(K, V);
+        newNode.next = node;
+        chainArray[chainIndex] = newNode;
+
+        //check if load factor is ok
+        if((1.0 * size) / M >= 0.75){
+
+            //create a temporarty copy of the hashtable
+            HashNode<K, V> temp[M];
+            for(int i = 0; i < size; i++){
+                temp[i] = chainArray[i];
+            }
+
+            //increase load factor
+            M *= 2;
+            tempSize = size;
+            size = 0;
+
+            //create new chainArray
+            chainArray = new HashNode[M];
+
+            //put old elements in new chainArray
+            for(int i = 0; i < tempSize; i++){
+                node = temp[i];
+                while(node != null){
+                    add(node.key, node.value);
+                    node = node.next;
+                }
             }
         }
-          */
     }
 
     public V get(K key) {

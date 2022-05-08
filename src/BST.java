ad92fabb607484d67ace0dadff99d8f3b8c60bd4
tuchaVshortah <nukenbai190104@gmail.com>
2022-05-08 14:12:44 +0600

    Add definitions for the void put(K key, V val) method

--- src/BST.java
+++ src/BST.java
@@ -5,14 +5,41 @@ public class BST<K extends Comparable<K>, V> {
     private class Node {
         private K key;
         private V value;
-        private Node left, right;
+        private Node left = null, right = null;
         public Node(K key, V val) {
             this.key = key;
             this.value = val;
         }
     }
 
-    public void put(K key, V val) {}
+    public void put(K key, V val) {
+        Node node = new Node(key, val), tempRoot = null;
+
+        if(root == null || root.key.compareTo(node.key) == 0){
+            root = node;
+            return;
+        }
+
+        tempRoot = root;
+        while(tempRoot.key.compareTo(node.key) == 1){
+            if(tempRoot.left != null){
+                tempRoot = tempRoot.left;
+            }else{
+                tempRoot.left = node;
+                return;
+            }
+        }
+
+        tempRoot = root;
+        while(tempRoot.key.compareTo(node.key) == -1){
+            if(tempRoot.right != null){
+                tempRoot = tempRoot.right;
+            }else{
+                tempRoot.right = node;
+                return;
+            }
+        }
+    }
 
     public V get(K key) {
         return null;

4a3a3f96ff771a6617c70a985db373c4a75a39fa
tuchaVshortah <nukenbai190104@gmail.com>
2022-05-09 21:35:03 +0600

    Add tests for hashtable

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,93 @@
 public class Main {
     public static void main(String[] args) {
-        BST<Integer, String> temp = new BST<>();
+
+        System.out.println("------Tests for BST implementation------\n");
+
+        BST<Integer, String> bst = new BST<>();
 
         System.out.println("Add elements to the tree\n");
 
-        temp.put(1, "1");
-        temp.put(0, "0");
-        temp.put(-2, "-2");
-        temp.put(-1, "-1");
-        temp.put(2, "2");
+        bst.put(1, "1");
+        bst.put(0, "0");
+        bst.put(-2, "-2");
+        bst.put(-1, "-1");
+        bst.put(2, "2");
 
-        System.out.println("Show elements in the tree\n");
+        System.out.println("Show values in the tree\n");
 
-        System.out.println(temp.get(1));
-        System.out.println(temp.get(0));
-        System.out.println(temp.get(-2));
-        System.out.println(temp.get(-1));
-        System.out.println(temp.get(2));
+        System.out.println(bst.get(1));
+        System.out.println(bst.get(0));
+        System.out.println(bst.get(-2));
+        System.out.println(bst.get(-1));
+        System.out.println(bst.get(2));
 
-        System.out.println("Printing keys from the BST iterable\n");
-        for(Integer key: temp.iterator()){
+        System.out.println("Printing keys from BST iterable\n");
+        for(Integer key: bst.iterator()){
             System.out.println(key);
         }
 
         System.out.println("Delete a few elements from the tree\n");
 
-        temp.delete(1);
-        temp.delete(0);
-        temp.delete(-2);
-        temp.delete(-1);
-        temp.delete(2);
+        bst.delete(1);
+        bst.delete(0);
+        bst.delete(-2);
+        bst.delete(-1);
+        bst.delete(2);
 
         System.out.println("Show the updated tree\n");
 
-        System.out.println(temp.get(1));
-        System.out.println(temp.get(0));
-        System.out.println(temp.get(-2));
-        System.out.println(temp.get(-1));
-        System.out.println(temp.get(2));
+        System.out.println(bst.get(1));
+        System.out.println(bst.get(0));
+        System.out.println(bst.get(-2));
+        System.out.println(bst.get(-1));
+        System.out.println(bst.get(2));
 
+        System.out.println("-----------------DONE-----------------\n");
+
+        System.out.println("------Tests for the HashTable implementation------\n");
+
+        MyHashTable<Integer, String> hashTable = new MyHashTable<>();
+
+        System.out.println("Add elements to hashtable\n");
+
+        hashTable.put(1, "one");
+        hashTable.put(2, "two");
+        hashTable.put(3, "three");
+        hashTable.put(4, "four");
+        hashTable.put(5, "five");
+
+        System.out.println("Show values in hashtable\n");
+
+        System.out.println(hashTable.get(1));
+        System.out.println(hashTable.get(2));
+        System.out.println(hashTable.get(3));
+        System.out.println(hashTable.get(4));
+        System.out.println(hashTable.get(5));
+
+        System.out.println("Show keys in hashtable\n");
+
+        System.out.println(hashTable.getKey("one"));
+        System.out.println(hashTable.getKey("two"));
+        System.out.println(hashTable.getKey("three"));
+        System.out.println(hashTable.getKey("four"));
+        System.out.println(hashTable.getKey("five"));
+
+        System.out.println("Delete keys from hashtable\n");
+
+        System.out.println(hashTable.remove(1));
+        System.out.println(hashTable.remove(2));
+        System.out.println(hashTable.remove(3));
+        System.out.println(hashTable.remove(4));
+        System.out.println(hashTable.remove(5));
+
+        System.out.println("Check if hashtable contains values\n");
+
+        System.out.println(hashTable.contains("one"));
+        System.out.println(hashTable.contains("two"));
+        System.out.println(hashTable.contains("three"));
+        System.out.println(hashTable.contains("four"));
+        System.out.println(hashTable.contains("five"));
+
+        System.out.println("-----------------DONE-----------------\n");
     }
 }
